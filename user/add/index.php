f52757e06fc9be658b1a8703830e9634eafeeeb0
lca311 <gspitzer@leoapp-slg.de>
2018-06-26 19:38:41 +0200

    Bugfix

--- user/add/index.php
+++ user/add/index.php
@@ -15,14 +15,14 @@
 
             $db = parent::getConnection();
 
-            $checksum = $_POST['checksum'];
-            $device = $db->real_escape_string($_POST['device'])
+            $checksum = $db->real_escape_string($_POST['checksum']);
+            $device = $db->real_escape_string($_POST['device']);
             $name = $db->real_escape_string($_POST['name']);
 
-            parent::exitOnBadRequest($checksum, $name);
+            parent::exitOnBadRequest($checksum, $name, $device);
 
-            if (verifyChecksum($checksum, $name)) {
-                parent::returnApiError("checksum does not match username", 400);
+            if (!verifyChecksum($checksum, $name)) {
+                parent::returnApiError("checksum not valid", 400);
             }
 
             $query = "SELECT uname, uid, uklasse FROM Users WHERE udefaultname = '$name'";
@@ -33,7 +33,7 @@
             }
 
             if($result->num_rows == 0) { //User doesn't exist yet and is created
-                $permission = $db->real_escape_string($_GET['permission']);
+                $permission = (strlen($name) == 6 ? 2 : 1);
                 $klasse = "N/A";
                 if($permission == 2) {
                     $klasse = "TEA";
@@ -46,8 +46,24 @@
                     parent::returnApiError("Internal Server Error", 500);
                 }
             }
+
+            $query = "SELECT uid FROM Users WHERE udefaultname = '$name'";
+            $result = $db->query($query);
+
+            if ($result === false) {
+                parent::returnApiError("Internal Server Error", 500);
+            }
+
+            $id = $result->fetch_assoc()['uid'];
+
+            $query = "INSERT INTO Devices VALUES ('$device', $id, '$checksum', null) ON DUPLICATE KEY UPDATE identifier = '$device'";
+            $result = $db->query($query);
         
-            parent::returnApiSuccess();
+            if ($result === false) {
+                parent::returnApiError("Internal Server Error", 500);
+            }
+
+            parent::returnApiResponse(array("user_id" => $id));
 
             $db->close();
         }
